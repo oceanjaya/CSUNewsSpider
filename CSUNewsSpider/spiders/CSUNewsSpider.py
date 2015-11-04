9f1b310d057064e1ed78a7d89df4d14bd632d889
OceanJaya <jiang.ocean@hotmail.com>
2015-11-05 04:31:05 +0800

    Complete the method to store dates into mysql

--- CSUNewsSpider/spiders/CSUNewsSpider.py
+++ CSUNewsSpider/spiders/CSUNewsSpider.py
@@ -1,12 +1,13 @@
 # coding=utf-8
+import datetime
 import scrapy
 import MySQLdb
-
-# ����ݿ�����
-
-# ʹ��cursor()������ȡ�����α�
+db=MySQLdb.connect("localhost","spider","xyz","csuspider")
 cursor = db.cursor()
-
+db.set_character_set('utf8')
+cursor.execute('SET NAMES utf8;')
+cursor.execute('SET CHARACTER SET utf8;')
+cursor.execute('SET character_set_connection=utf8;')
 
 class CSUNewsSpider(scrapy.Spider):
     name="CSUNews"
@@ -22,29 +23,27 @@ class CSUNewsSpider(scrapy.Spider):
     def parse_links_content(self,response):
         try:
             contents=''
-            title=''
-            time=''
             for content in response.css('.subCont p::text').extract():
                 contents+=content
-            title+=response.css('.subTitle2 span::text').extract()[0]
-            time+='-'.join(response.css('.otherTme::text').re(r'(\d+)'))
-            # SQL �������
-            sql = """INSERT INTO EMPLOYEE(title,content, time)VALUES ('%s', '%s','%s')"""%(title,contents,time)
-            try:
-                # ִ��sql���
-                cursor.execute(sql)
-                # �ύ����ݿ�ִ��
-                db.commit()
-            except:
-                # Rollback in case there is any error
-                db.rollback()
-        # �ر���ݿ�����
-      	        db.close()
+            title=response.css('.subTitle2 span::text').extract()[0]
+            date='-'.join(response.css('.otherTme::text').re(r'(\d+)'))
+            url=response.url
 
             yield{
                 'title':title,
-                'time' :time,
+                'date' :date,
                 'contents':contents,
+                'url':url,
             }
-        except:
+            sql = """insert into news(title,content, date,url) values ('%s', '%s','%s','%s')"""\
+                  %(title.encode('utf-8'),contents.encode('utf-8'),date.encode('utf-8'),url.encode('utf-8'))
+            try:
+                cursor.execute(sql)
+                db.commit()
+            except Exception,e:
+                print e
+                db.rollback()
+                db.close()
+        except Exception,e:
+            print e
             pass
